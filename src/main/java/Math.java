cdf35d79eb6ba009e69fa90308305989069b682b
unknown <kewel2010.mb@gmail.com>
2020-06-10 22:36:24 -0400

    primer comit antes de subir

---
+++ src/main/java/Math.java
@@ -0,0 +1,18 @@
+
+public class Math {
+
+	
+//	public static void main(String args []) {
+//		System.out.println("Hola mundo");
+//	}
+//	
+	/***
+	 * Suma
+	 * @param x
+	 * @param y
+	 * @return entero de x + y
+	 */
+	public static int suma(int x, int y) {
+		return 2;
+	}
+}
---
+++ src/test/java/MathTest.java
@@ -0,0 +1,17 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathTest {
+
+	@Test
+	public void testSuma() {
+		assertEquals(2,Math.suma(1,1) );
+	}
+//	@Test
+//	public void testSuma2() {
+//		assertEquals(2, Math.suma(1,1));
+//	}
+
+}
